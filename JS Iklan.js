f25dcd58e75a279d3e41fa0f13812ac1c7cad1f4
firmanriawan9 <31408277+firmanriawan9@users.noreply.github.com>
2017-10-22 18:28:19 +0700

    Add files via upload

---
+++ JS Iklan.js
@@ -0,0 +1,8 @@
+// JavaScript Document //<br />
+ads = new Array(4);<br />
+ads [0] = ‘</p>
+<div class="ads-box" "item"><a href="https://goo.gl/ef6dev" target="blank"><img src="https://2.bp.blogspot.com/-OLvT54mOndI/Wenvqr6E1fI/AAAAAAAAAmQ/CoguPEkidjA8BHEMujcg6-E_n0mVuzgqgCEwYBhgL/s1600/4635467333049326180.jpg" data-recalc-dims="1"></a></p>
+<p class="ad"><a href="http://androedland.net" target="blank"><span><font color="#000000" size="1" face="Arial, Helvetica, sans-serif">ads by Androedland</font></span></a></p>
+</div>
+<p>‘;<br />
+view = Math.floor(Math.random() * 1); document.write(ads[view]);
